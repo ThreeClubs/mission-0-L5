ae50a3c1fe2ce3ec6b5a97931935e68bd9655f34
Andrew Grenon <andrew.grenon@gmail.com>
2022-11-10 16:10:49 +1300

    init commit mission-0 template from Scrimba

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div
+      id="footerContainer"
+      className="bg-gray-200 font-sans flex flex-col items-center"
+    >
+      <footer class="w-full bg-gray-800 pb-8 text-center text-xs font-light text-gray-600">
+        <p>Copyright &copy; 2022 Andrew Grenon</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
