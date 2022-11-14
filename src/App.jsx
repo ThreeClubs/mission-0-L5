5ceca174fe440be29fc924210d413284bc4d88dd
Andrew Grenon <andrew.grenon@gmail.com>
2022-11-14 14:31:32 +1300

    changed RRD comp to elem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../src/pages/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" component={Home} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 );
